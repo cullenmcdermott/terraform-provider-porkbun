e28edd87e5f784cf74aade04daca1c64191ffa58
Jenia Varavva <fuzzy.parabola@gmail.com>
2023-07-03 22:09:15 -0700

    Fix error handling of non-retriable HTTP errors

--- main.go
+++ main.go
@@ -36,7 +36,7 @@ func main() {
 
 	opts := providerserver.ServeOpts{
 		// TODO: Update this string with the published name of your provider.
-		Address: "registry.terraform.io/hashicorp/scaffolding",
+		Address: "registry.terraform.io/cullenmcdermott/porkbun",
 		Debug:   debug,
 	}
 
